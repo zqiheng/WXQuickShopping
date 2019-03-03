3b603c21d4b8851f4dab8a1f15a3334cf5ae22f9
qiheng.zhang <qiheng.zhang@fa-software.com>
2019-03-03 14:15:32 +0800

    主题：个人中心页面设计

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,88 +1,89 @@
-// pages/my/my.js
-const app = getApp() // 获取应用实例
+var app = getApp()
 
-// page/component/new-pages/user/user.js
 Page({
   data: {
-    thumb: '',
-    nickname: '',
-    orders: [],
-    hasAddress: false,
-    address: {}
-  },
-  Recharge: function() {
-    var that = this
-    wx.showModal({
-      title: '消息',
-      content: '该功能尚未开放',
-      success: function(res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-        }
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    orderItems: [
+      {
+        typeId: 0,
+        name: '待付款',
+        url: 'bill',
+        imageurl: '../../images/person/personal_pay.png',
+      },
+      {
+        typeId: 1,
+        name: '待收货',
+        url: 'bill',
+        imageurl: '../../images/person/personal_receipt.png',
+      },
+      {
+        typeId: 2,
+        name: '待评价',
+        url: 'bill',
+        imageurl: '../../images/person/personal_comment.png'
+      },
+      {
+        typeId: 3,
+        name: '退换/售后',
+        url: 'bill',
+        imageurl: '../../images/person/personal_service.png'
       }
+    ],
+  },
+
+
+  //事件处理函数
+  toOrder: function (e) {
+    console.log(e);
+    wx.redirectTo({
+      url: '../order/order'
     })
   },
-  onLoad() {
-    var self = this;
-    /**
-     * 获取用户信息
-     */
-    wx.getUserInfo({
-        success: function(res) {
-          self.setData({
-            thumb: res.userInfo.avatarUrl,
-            nickname: res.userInfo.nickName
-          })
-        }
-      }),
 
-      /**
-       * 发起请求获取订单列表信息
-       */
-      wx.request({
-        url: '',
-        success(res) {
-          self.setData({
-            orders: res.data
+  // 页面加载时动作
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
           })
         }
       })
+    }
   },
-  onShow() {
-    var self = this;
-    /**
-     * 获取本地缓存 地址信息
-     */
-    wx.getStorage({
-      key: 'address',
-      success: function(res) {
-        self.setData({
-          hasAddress: true,
-          address: res.data
-        })
-      }
+
+  // 获取用户信息
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   },
-  /**
-   * 发起支付请求
-   */
-  payOrders() {
-    wx.requestPayment({
-      timeStamp: 'String1',
-      nonceStr: 'String2',
-      package: 'String3',
-      signType: 'MD5',
-      paySign: 'String4',
-      success: function(res) {
-        console.log(res)
-      },
-      fail: function(res) {
-        wx.showModal({
-          title: '支付提示',
-          content: '<text>',
-          showCancel: false
-        })
-      }
-    })
+
+  // 我的地址
+  myAddress: function (e) {
+    wx.navigateTo({ url: '../addressList/addressList' });
   }
 })
